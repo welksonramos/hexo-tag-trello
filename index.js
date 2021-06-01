569086c7f655cca17954541c5ee4b1a6914d01c6
Welkson Ramos <welksonr@hotmail.com>
2021-05-31 23:46:44 -0300

    chore: extends hexo injector trello

--- index.js
+++ index.js
@@ -2,3 +2,5 @@
 
 // eslint-disable-next-line no-undef
 hexo.extend.tag.register("trello", require("./lib/render")(hexo), {async: true, end: true});
+
+hexo.extend.injector.register("body_end", require("./lib/inject/trello")(hexo));
